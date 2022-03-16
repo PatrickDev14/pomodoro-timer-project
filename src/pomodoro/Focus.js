21f129f79bf5ac99c9c354584c1bc9f39703b597
Patrick Lauher <plauher14@gmail.com>
2022-03-16 15:43:05 -0500

    add Focus.js and Break.js components

---
+++ src/pomodoro/Focus.js
@@ -0,0 +1,39 @@
+
+import React from "react"
+import {minutesToDuration} from "../utils/duration"
+
+function Focus({focusIncrease, focusDecrease, focusDuration, isTimerRunning}) {
+    return (
+    <div className="col">
+          <div className="input-group input-group-lg mb-2">
+            <span className="input-group-text" data-testid="duration-focus">
+              Focus Duration: {minutesToDuration(focusDuration)}
+            </span>
+            <div className="input-group-append">
+              {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-testid="decrease-focus"
+                onClick = {focusDecrease}
+                disabled = {isTimerRunning}
+              >
+                <span className="oi oi-minus" />
+              </button>
+              {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-testid="increase-focus"
+                onClick = {focusIncrease}
+                disabled = {isTimerRunning}
+              >
+                <span className="oi oi-plus" />
+              </button>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Focus;
