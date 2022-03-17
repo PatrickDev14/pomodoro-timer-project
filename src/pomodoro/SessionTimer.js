1046e88e15ecf6bcb01d9a967efa731271d09c67
Patrick Lauher <plauher14@gmail.com>
2022-03-17 00:26:54 -0500

    add SessionTimer component

---
+++ src/pomodoro/SessionTimer.js
@@ -0,0 +1,46 @@
+
+import React from "react"
+import { minutesToDuration, secondsToDuration } from '../utils/duration';
+
+function SessionTimer({session, focusDuration, breakDuration, progressBarCurrent}) {
+
+    return (
+      <div>
+        {/* TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
+        {session && (
+          <div>
+            <div className="row mb-2">
+              <div className="col">
+                {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
+                <h2 data-testid="session-title">
+                {session?.label} for {session?.label === "Focusing"
+                  ? minutesToDuration(focusDuration)
+                  : minutesToDuration(breakDuration)}
+                {" "} minutes
+                </h2>
+                {/* TODO: Update message below correctly format the time remaining in the current session */}
+                <p className="lead" data-testid="session-sub-title">
+                  {secondsToDuration(session?.timeRemaining)} remaining
+                </p>
+              </div>
+            </div>
+            <div className="row mb-2">
+              <div className="col">
+                <div className="progress" style={{ height: "20px" }}>
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    aria-valuenow={progressBarCurrent} // TODO: Increase aria-valuenow as elapsed time increases
+                    style={{ width: `${progressBarCurrent}%` }} // TODO: Increase width % as elapsed time increases
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    )
+}
+export default SessionTimer;
