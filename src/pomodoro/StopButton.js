a511ed9416386fcc8d55b15eefec6911ded324aa
Patrick Lauher <plauher14@gmail.com>
2022-03-19 00:45:49 -0500

    add PlayPauseButtons.js and StopButton.js

---
+++ src/pomodoro/StopButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function StopButton ({ isTimerRunning, stopButtonHandler }) {
+    return (
+        <div>
+        {/* TODO: Implement stopping the current focus or break session. and disable the stop button when there is no active session */}
+        {/* TODO: Disable the stop button when there is no active session */}
+            <button
+                type="button"
+                className="btn btn-secondary"
+                data-testid="stop"
+                title="Stop the session"
+                disabled={!isTimerRunning}
+                onClick={stopButtonHandler}
+              >
+            <span className="oi oi-media-stop" />
+            </button>
+        </div>
+    )
+}
