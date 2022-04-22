c8353d6339752f7d5be8cb22c705c106e9b9ba94
Patrick Lauher <plauher14@gmail.com>
2022-04-21 19:15:54 -0500

    pushing build folder for deploy

---
+++ build/precache-manifest.6cac0dd3ae778b2eb7d05a9224c497fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96574628a5bbae6c5b6d6eb899d9bd68",
+    "url": "/pomodoro-timer-project/index.html"
+  },
+  {
+    "revision": "b3779f1ef470843aaebf",
+    "url": "/pomodoro-timer-project/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/pomodoro-timer-project/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer-project/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3779f1ef470843aaebf",
+    "url": "/pomodoro-timer-project/static/js/main.8c4017f5.chunk.js"
+  },
+  {
+    "revision": "462caf3c29702740d782",
+    "url": "/pomodoro-timer-project/static/js/runtime-main.495a66d1.js"
+  }
+]);
